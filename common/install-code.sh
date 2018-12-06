c8e90c981d3731d222ed7042576a35978795c0b3
Henrik Hedlund <henrik@hedlund.im>
2018-12-06 20:38:32 +0100

    Updating Code plugins (disabling most of them for now).

--- common/install-code.sh
+++ common/install-code.sh
@@ -5,28 +5,27 @@ if [ -z "$(which code)" ]; then
 else
 
   # Extensions
-  code --install-extension christian-kohler.npm-intellisense
-  code --install-extension christian-kohler.path-intellisense
-  code --install-extension dbaeumer.vscode-eslint
-  code --install-extension deerawan.vscode-dash
-  code --install-extension eamodio.gitlens
-  code --install-extension EditorConfig.EditorConfig
-  code --install-extension esbenp.prettier-vscode
-  code --install-extension formulahendry.auto-close-tag
-  code --install-extension formulahendry.auto-rename-tag
-  code --install-extension mitchdenny.ecdc
-  code --install-extension msjsdiag.debugger-for-chrome
-  code --install-extension wmaurer.change-case
+  # code --install-extension christian-kohler.npm-intellisense
+  # code --install-extension christian-kohler.path-intellisense
+  # code --install-extension dbaeumer.vscode-eslint
+  # code --install-extension deerawan.vscode-dash
+  # code --install-extension eamodio.gitlens
+  # code --install-extension EditorConfig.EditorConfig
+  # code --install-extension esbenp.prettier-vscode
+  # code --install-extension formulahendry.auto-close-tag
+  # code --install-extension formulahendry.auto-rename-tag
+  # code --install-extension mitchdenny.ecdc
+  # code --install-extension msjsdiag.debugger-for-chrome
+  # code --install-extension wmaurer.change-case
   
   # Languages
-  code --install-extension DotJoshJohnson.xml
-  code --install-extension geequlim.godot-tools
-  code --install-extension lukehoban.Go
   code --install-extension mauve.terraform
+  code --install-extension ms-vscode.Go
   code --install-extension ms-vscode.PowerShell
-  code --install-extension PeterJausovec.vscode-docker
+  # code --install-extension DotJoshJohnson.xml
+  # code --install-extension geequlim.godot-tools
+  # code --install-extension PeterJausovec.vscode-docker
   
   # Themes
-  code --install-extension akamud.vscode-theme-onedark
-  code --install-extension file-icons.file-icons
+  code --install-extension Equinusocio.vsc-material-theme
 fi
