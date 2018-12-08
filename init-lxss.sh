d164dd2555a46188b86229c369dc20be88f9cbef
Henrik Hedlund <henrik@hedlund.im>
2018-12-08 16:42:09 +0100

    Symlink Projects and Temp from Windows into LXSS

--- init-lxss.sh
+++ init-lxss.sh
@@ -13,3 +13,12 @@ ${SCRIPT_DIR}/common/install-node.sh
 
 # Install some Golang tools
 ${SCRIPT_DIR}/common/install-golang.sh
+
+# Symlink the Windows folders
+WINDOWS_USER=$(\ls /mnt/c/Users | grep 'henrik\?')
+if [ ! -z "$WINDOWS_USER" ]; then
+  [ -d "/mnt/c/Users/${WINDOWS_USER}/Projects" ] && ln -sf "/mnt/c/Users/${WINDOWS_USER}/Projects" "${HOME}/Projects"
+  [ -d "/mnt/c/Users/${WINDOWS_USER}/Temp" ] && ln -sf "/mnt/c/Users/${WINDOWS_USER}/Temp" "${HOME}/Temp"
+else
+  echo "Unable to locate Windows user. Skipping folder linking!"
+fi
