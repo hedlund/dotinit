c6903d8aa0c14762eeeac3c04d340e6f1fff3eac
Henrik Hedlund <henrik@hedlund.im>
2019-05-21 19:09:55 +0200

    Add Golang libraries

--- common/install-golang.sh
+++ common/install-golang.sh
@@ -8,13 +8,18 @@ else
   [ ! -d "$GOPATH/src" ] && mkdir -p "$GOPATH/src"
 
   go get -u github.com/kardianos/govendor
-#   go get -u github.com/ramya-rao-a/go-outline
-#   go get -u github.com/nsf/gocode
-#   go get -u github.com/uudashr/gopkgs/cmd/gopkgs
-#   go get -u github.com/acroca/go-symbols
-#   go get -u golang.org/x/tools/cmd/guru
-#   go get -u golang.org/x/tools/cmd/gorename
-#   go get -u github.com/rogpeppe/godef
-#   go get -u sourcegraph.com/sqs/goreturns
-#   go get -u github.com/golang/lint/golint
+  go get -u github.com/cespare/reflex
+
+  # Dependencies used by vscode-go
+  go get -u github.com/mdempsky/gocode
+  go get -u github.com/uudashr/gopkgs/cmd/gopkgs
+  go get -u github.com/ramya-rao-a/go-outline
+  go get -u github.com/acroca/go-symbols
+  go get -u golang.org/x/tools/cmd/guru
+  go get -u golang.org/x/tools/cmd/gorename
+  go get -u github.com/go-delve/delve/cmd/dlv
+  go get -u github.com/stamblerre/gocode
+  go get -u github.com/rogpeppe/godef
+  go get -u github.com/sqs/goreturns
+  go get -u golang.org/x/lint/golint
 fi
