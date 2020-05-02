f8a5e2e2e7e6c4d5d91359eba749303baa7951c7
Henrik Hedlund <henrik@hedlund.im>
2020-05-02 18:28:41 +0200

    Switch from nvm to n!

--- common/install-node.sh
+++ common/install-node.sh
@@ -4,15 +4,16 @@
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "${SCRIPT_DIR}/helpers.sh"
 
-if ! exists nvm; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-  
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# Install n! & update node
+if ! exists n; then
+  echo "Installing n!..."
+  export N_PREFIX="$HOME/.n"
+  export PATH="$N_PREFIX/bin:$PATH"
+  mkdir -p "$N_PREFIX"
+  sudo npm install -g n
 
-  # Install the latest version of node
-  nvm install node
+  # Install latest version of node
+  n latest
 fi
 
 if ! exists npm; then
@@ -21,6 +22,5 @@ else
   npm install -g depcheck
   npm install -g serve
   npm install -g typescript
-  npm install -g yo
   npm install -g nodemon
 fi
